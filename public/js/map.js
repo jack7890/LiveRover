728323dc06ef89095b68b24ce1eb87a74acb1396
Jack Groetzinger <jackgretz@gmail.com>
2012-02-20 17:01:54 -0500

    Added tooltip for date at top

---
+++ public/js/map.js
@@ -0,0 +1,7 @@
+$(function() {
+  $('#date-value').popover({
+    placement:  'bottom',
+    title:      'Have a certain date in mind?',
+    content:    'Click above to enter a custom date.  For example "next saturday" or "aug 14".' 
+  });
+});
