80973592b03340820a25d85b2744bd3186b07dfa
Jack Groetzinger <jackgretz@gmail.com>
2012-02-21 23:07:10 -0500

    Formatting fix

--- public/js/app.js
+++ public/js/app.js
@@ -203,7 +203,7 @@ $(function() {
             link:       that.model.attributes.url,
             venue:      that.model.attributes.venue.name,
             low:        that.model.attributes.stats.lowest_price,
-            count:        that.model.attributes.stats.listing_count,            
+            count:      that.model.attributes.stats.listing_count,            
             time:       Date.parse(that.model.attributes.datetime_local).toString("ddd, MMM d @ h:mmtt")
           });
       return content;
