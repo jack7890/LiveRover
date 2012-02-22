93c714cb03a2d8adba22ba839541913b30f5707c
Jack Groetzinger <jackgretz@gmail.com>
2012-02-21 20:57:49 -0500

    Closes #8

--- public/js/map.js
+++ public/js/map.js
@@ -4,4 +4,7 @@ $(function() {
     title:      'Have a certain date in mind?',
     content:    'Click above to enter a custom date.  For example "next saturday" or "aug 14".' 
   });
+  $('.date-input').bind('keyup', function(e) {
+    if(e.keyCode === 10 || e.keyCode === 13) $('.date-input').blur();
+  });
 });
